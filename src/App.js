6e13924ae1e8cbb20539b58834d0d5dd14f55776
Maksym Hrom <mhrom@deloitte.com>
2022-08-20 17:10:52 +0300

    added firebase db and TODO form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import {doc, addDoc, getDocs, deleteDoc, updateDoc } from "firebase/firestore";
+import {db, todosCollection} from "./fb";
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [text, setText] = useState("");
+    const [todos, setTodos] = useState([]);
+    const [disabled, setDisabled] = useState(false);
+    const getData = async () => {
+        const querySnapshot = await getDocs(todosCollection);
+        const newTodos = [];
+        querySnapshot.forEach(todo => {
+            newTodos.push({
+                id: todo.id,
+                ...todo.data()
+            });
+        })
+        setTodos(newTodos);
+    }
+
+    useEffect(() => {
+        getData()
+    }, []);
+
+    const addTodo = async () => {
+        if (!text) {
+            return;
+        }
+        setDisabled(true);
+        await addDoc(todosCollection, { text, isDone: false });
+        await getData();
+        setText('');
+        setDisabled(false);
+    }
+
+    const deleteTodo = async (id) => {
+        setDisabled(true);
+        await deleteDoc(doc(db, 'todos', id));
+        await getData();
+        setDisabled(false);
+    }
+
+    const toggleTodo = async (id, isDone) => {
+        setDisabled(true);
+        await updateDoc(doc(db, 'todos', id), { isDone });
+        await getData();
+        setDisabled(false);
+    }
+
+    return (
+        <div>
+            <input disabled={disabled} type="text" value={text} onInput={(event) => setText(event.target.value)} />
+            <button disabled={disabled} onClick={() => addTodo()}>Add Todo</button>
+            <div>
+                <ol>
+                    {todos.map(({id, text, isDone}) => (
+                        <li key={id}>
+                            <input type="checkbox" checked={isDone} onChange={() => toggleTodo(id, !isDone)}/> {text} ->
+                            <button onClick={() => deleteTodo(id)}>DELETE</button></li>
+                    ))}
+                </ol>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/fb.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {collection, getFirestore} from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "<SECRET_KEY>",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+export const db = getFirestore(app);
+
+export const todosCollection = collection(db, 'todos');
